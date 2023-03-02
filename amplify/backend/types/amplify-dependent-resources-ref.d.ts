84e70b8f386be4eeb83f00d7aa0e25027ff77fd0
kevingmny <you@example.comkevingmny23@gmail.com>
2023-03-02 17:17:08 +0100

    Fino a qui l'app dispone dell'autorizzazione soltanto e funziona

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "maseappea5eb257": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
