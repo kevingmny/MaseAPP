4ee795716952a0078601f2b76b06f0754addf471
kevingmny <you@example.comkevingmny23@gmail.com>
2023-03-03 19:18:33 +0100

    Adesso è possibile tramite ID cercare il marittimo e viene restituito a schermo con i suoi attributi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,4 @@
-/*import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator} from '@aws-amplify/ui-react';
-//import { AmplifySignOut } from '@aws-amplify/ui-react-v1'; da provare a implementare
-import { Seafarer } from './models';
-
-function App({signOut}) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Benvenuti nella mia applicazione!
-        </p>
-        <button onClick={signOut}>Sign out</button>
-      </header>
-    </div>
-  );
-}
-
-export default withAuthenticator(App);*/
-
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { API } from 'aws-amplify';
@@ -31,30 +7,33 @@ import { Seafarer } from './models';
 
 function App({ signOut }) {
   const [seafarer, setSeafarer] = useState(null);
+  const [seafarerId, setSeafarerId] = useState("");
 
-  useEffect(() => {
-    async function fetchSeafarer() {
-      try {
-        const response = await API.graphql({
-          query: getSeafarer,
-          variables: { id: '64cb43e7-ded0-40dd-9175-8cfcf09e0ca8' } // replace with actual seafarer ID
-        });
-        const seafarerData = response.data.getSeafarer;
-        setSeafarer(seafarerData);
-      } catch (error) {
-        console.log('Error fetching seafarer', error);
-      }
+  const handleSearch = async () => {
+    try {
+      const response = await API.graphql({
+        query: getSeafarer,
+        variables: { id: seafarerId }
+      });
+      const seafarerData = response.data.getSeafarer;
+      setSeafarer(seafarerData);
+    } catch (error) {
+      console.log('Error fetching seafarer', error);
     }
-    fetchSeafarer();
-  }, []);
+  };
+
+//TEST: use '64cb43e7-ded0-40dd-9175-8cfcf09e0ca8' as ID in front-end input form (cerca)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Benvenuti nella mia applicazione!
-        </p>
+        </h1>
+        <div>
+          <input id="seafarerIdInput" type="text" value={seafarerId} onChange={(event) => setSeafarerId(event.target.value)} />
+          <button onClick={handleSearch} >Cerca</button>
+        </div>
         {seafarer ? (
           <div>
             <h2>{seafarer.name} {seafarer.surname}</h2>
@@ -69,14 +48,14 @@ function App({ signOut }) {
             <p>Numero di registrazione: {seafarer.registration_number}</p>
             <p>Indirizzo: {seafarer.address}, {seafarer.city_of_residence} ({seafarer.zip_code})</p>
             <p>Telefono: {seafarer.telephone_number}</p>
-            <p>Email: {seafarer.username}</p>
+            <p>Username: {seafarer.username}</p>
             <p>Tipo di abbonamento: {seafarer.subscription_type}</p>
             <p>Data di creazione: {new Date(seafarer.createdAt).toLocaleString()}</p>
             <p>Data di ultima modifica: {new Date(seafarer.updatedAt).toLocaleString()}</p>
             <img src={seafarer.profile_image} alt="Foto profilo" />
           </div>
         ) : (
-          <p>Loading seafarer data...</p>
+          <p>Inserisci l'id del marittimo da cercare.</p>
         )}
         <button onClick={signOut}>Sign out</button>
       </header>
@@ -84,12 +63,11 @@ function App({ signOut }) {
   );
 }
 
-function calculateAge(birthday) { // Funzione per calcolare l'età dal giorno di nascita
-    const ageDiff = Date.now() - new Date(birthday);
-    const ageDate = new Date(ageDiff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
+function calculateAge(birthday) { // Function to calculate age
+  const ageDiff = Date.now() - new Date(birthday);
+  const ageDate = new Date(ageDiff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+} 
 export default withAuthenticator(App);
 
 
